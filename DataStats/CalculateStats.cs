9d8a5ad5a1660976e0089dc0925f2a38f0c6f1b2
J.D. Sandifer <sandifer.jd@gmail.com>
2016-11-02 16:31:48 -0700

    Create initial commit (basic file reading test)

---
+++ DataStats/CalculateStats.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace JDSandifer.DataStats
+{
+    class CalculateStats
+    {
+        static void Main(string[] args)
+        {
+            /* Read in shortdata1.data and print out the line
+             * or an error */
+            string path = "shortdata1.data";
+            string numbers = "   Error reading file: no data";
+
+            if (File.Exists(path))
+            {
+                numbers = File.ReadAllLines(path)[0];
+            }
+
+            // Split the line by spaces and assign it to an array
+
+
+            // Print out the first line
+            Console.WriteLine(path);
+            Console.WriteLine(numbers);
+        }
+    }
+}

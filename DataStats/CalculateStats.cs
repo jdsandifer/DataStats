3ebb8890c30cac20162d9d366efb832c638bf611
J.D. Sandifer <sandifer.jd@gmail.com>
2016-11-06 09:26:17 -0800

    Clean up code formatting

--- DataStats/CalculateStats.cs
+++ DataStats/CalculateStats.cs
@@ -1,18 +1,14 @@
-﻿/* Copyright 2016 J.D. Sandifer - MIT License
- * 
- * Solution to the interview code problem
- * 
- * Because of the relatively small size of the task and its disconnected nature,
- * I chose not to use a bunch of objects, structs, helper classes, etc. so I
- * could reduce overhead as I would when on the job. Had this been a
- * component in a larger code base that might have benefited from more 
- * code reuse or separation of concerns, then I would have chosen a more
- * segmented and object oriented approach to help with that.
- * 
- * However, the stats calculations were getting nested pretty deeply so
- * I separated them into a different function. To me, fewer indents
- * means more readable code so I avoided several indents on some of the more
- * "meaty" code that way. */
+﻿// Copyright 2016 J.D. Sandifer - MIT License
+ 
+// Solution to the interview code problem
+
+// Because of the relatively small size of the task and its disconnected nature, I chose not 
+// to segment the code into objects, structs, single-purpose functions, etc. to keep the code 
+// easy to read without jumping around. Had this been a component in a larger code base that 
+// might have benefited from more code reuse or been difficult to test without OOP, then I would 
+// have chosen a more segmented and object oriented approach. I also "unit" tested this project
+// using .data files with descriptive names that held edge cases, error values, bad data, and 
+// large data sets. That way the tests just show up in the program output with the other data.
 
 using System;
 using System.Globalization;
@@ -29,16 +25,16 @@ namespace JDSandifer.DataStats
             const int Success = 0;
             const int ErrorNoDataFiles = 1;
             
-
-            /* Create an array of all .data files in the this folder */
+            // Create an array of all .data files in the this folder
             string[] fileNames = null;
 
             try
             {
                 fileNames = Directory.GetFiles(".", "*.data");
             }
-            catch (Exception e)
+            catch
             {
+                // We could potentially log errors here.
             }
 
             if (fileNames == null || fileNames.Length == 0)
@@ -48,10 +44,9 @@ namespace JDSandifer.DataStats
                 Console.ReadKey();
                 return ErrorNoDataFiles;
             }
-
-
-            /* Check for illegal characters, bad formatting, and then
-             * print the stats if everything's ok */
+            
+            // Check for illegal characters, bad formatting, and then
+            // print the stats if everything's ok
             foreach (string fileName in fileNames)
             {
                 // Print filename we're processing, but trim off the ".\" prefix
@@ -60,17 +55,15 @@ namespace JDSandifer.DataStats
                 // Read in the file and check for errors
                 string[] linesFromFile = File.ReadAllLines(fileName);
                 const string IllegalCharacters = "[^0-9. -]";
-                const string CorrectNumberList =
-                    "^[ ]?(-?[0-9]*[.]?[0-9]+)([ ]-?[0-9]*[.]?[0-9]+)*[ ]?";
+                const string CorrectNumberList = 
+                                "^[ ]?(-?[0-9]*[.]?[0-9]+)([ ]-?[0-9]*[.]?[0-9]+)*[ ]?";
 
-                if (linesFromFile == null 
-                    || linesFromFile.Length == 0
-                    || linesFromFile[0] == "")
+                if (linesFromFile == null || linesFromFile.Length == 0 || linesFromFile[0] == "")
                 {
                     Console.WriteLine("   Error reading file: no data");
                 }
-                else if (linesFromFile.Length != 1
-                    || Regex.IsMatch(linesFromFile[0], IllegalCharacters))
+                else if (linesFromFile.Length != 1 
+                            || Regex.IsMatch(linesFromFile[0], IllegalCharacters))
                 {
                     Console.WriteLine("   Error reading file: unexpected character");
                 }
@@ -107,13 +100,12 @@ namespace JDSandifer.DataStats
             Console.ReadKey();
             return Success;
         }
-
-
-        /* Prints out stats based on a given array of numbers.
-         * Expects at least one number in the array. */
+        
+        // Prints out stats based on a given array of numbers.
+        // Expects at least one number in the array.
         private static void CalculateAndPrintFileStats(double[] numbers)
         {
-            // At least one number is expected so initialize sum, min, and max
+            // At least one number is expected so initialize sum, min, and max with it
             decimal sum = (decimal) numbers[0];
             double min = numbers[0];
             double max = numbers[0];
@@ -139,24 +131,19 @@ namespace JDSandifer.DataStats
             foreach (double number in numbers)
             {
                 accurateNumber = (decimal) number;
-                squaresOfDifferences += (decimal) Math.Pow(
-                    (double)(accurateNumber - average), 2.0);
+                squaresOfDifferences += (decimal) Math.Pow((double)(accurateNumber - average), 2.0);
             }
 
-            standardDeviation = (decimal) Math.Pow(
-                (double) (squaresOfDifferences / (decimal) quantityOfNumbers),
-                0.5);
+            standardDeviation = 
+                (decimal) Math.Pow((double) (squaresOfDifferences / (decimal) quantityOfNumbers), 0.5);
             
-            /* Print out the stats */
+            // Print out the stats
             const string NumberFormat = "0.##";
-            
             Console.WriteLine("   Sum: " + sum.ToString(NumberFormat));
             Console.WriteLine("   Min: " + min.ToString(NumberFormat));
             Console.WriteLine("   Max: " + max.ToString(NumberFormat));
             Console.WriteLine("   Average: " + average.ToString(NumberFormat));
-            Console.WriteLine("   Standard Deviation: "
-                + standardDeviation.ToString(NumberFormat));
-            
+            Console.WriteLine("   Standard Deviation: " + standardDeviation.ToString(NumberFormat));
         }
     }
 }
